ec4fe28f3221182ce66ddaf7b76a87bf61d9a112
Shivam Yadav <shivamydv9889@gmail.com>
2026-07-07 16:02:11 +0530

    cors

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -21,7 +21,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5175",
       "http://localhost:3000",
-      "https://your-frontend.vercel.app",
+     "https://auth-system-xi-three.vercel.app"
     ],
     credentials: true,
   })
